965b1276bd7ea641411cba799a3f8c97b54c446c
fullonrager <45670806+fullonrager@users.noreply.github.com>
2020-12-29 14:36:59 +0000

    Update to v1.0.3

--- rtel3.py
+++ rtel3.py
@@ -6,22 +6,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from random import randint
 from collections import Counter
 import time
 from bs4 import BeautifulSoup
+import codecs
 import re
 import requests
 import sys
 import os
 
-# ***** USER-CONTROLLED VARIABLES *****
-
-# Set to 'True' to prevent GUI window, currently may prevent the decryptor from functioning.
-headless = False
-
-# *************************************
 os.system('cls')
-print("***  RTÉ Player Downloader (rte-L3 v1.0.2)  ***")
+print("***  RTÉ Player Downloader (rte-L3 v1.0.3)  ***")
 print("***        Developed by fullonrager         ***")
 print()
 
@@ -35,37 +31,43 @@ def yes_or_no(question):
 
 def download_video():
     print("Downloading video...")
-    os.system('python -m youtube_dl --fixup never -f bestvideo --output "temp/'+video_title+'.mp4" ' + '"'+video_mpd+'"')
+    os.system('python -m youtube_dl --fixup never -f bestvideo --output "temp/'+temp_title+'.mp4" ' + '"'+video_mpd+'"')
     print("Video downloaded successfully.")
     print("Downloading audio...")
-    os.system('python -m youtube_dl --fixup never -f bestaudio --output "temp/'+video_title+'.m4a" ' + '"'+video_mpd+'"')
+    os.system('python -m youtube_dl --fixup never -f bestaudio --output "temp/'+temp_title+'.m4a" ' + '"'+video_mpd+'"')
     print("Audio downloaded successfully.")
 
 def cleanup():
     print("Removing leftover files...")
-    if os.path.isfile('temp/'+video_title+'.mp4.part_urls'):
-        os.remove('temp/'+video_title+'.mp4.part_urls')
-    if os.path.isfile('temp/'+video_title+'.m4a.part_urls'):
-        os.remove('temp/'+video_title+'.m4a.part_urls')
-    if os.path.isfile('temp/'+video_title+'.mp4.part_urls.txt'):
-        os.remove('temp/'+video_title+'.mp4.part_urls.txt')
-    if os.path.isfile('temp/'+video_title+'.m4a.part_urls.txt'):
-        os.remove('temp/'+video_title+'.m4a.part_urls.txt')
-    if os.path.isfile('temp/'+video_title+'.m4a'):
-        os.remove('temp/'+video_title+'.m4a')
-    if os.path.isfile('temp/'+video_title+'.mp4'):
-        os.remove('temp/'+video_title+'.mp4')
+    if os.path.isfile('temp/'+temp_title+'.mp4.part_urls'):
+        os.remove('temp/'+temp_title+'.mp4.part_urls')
+    if os.path.isfile('temp/'+temp_title+'.m4a.part_urls'):
+        os.remove('temp/'+temp_title+'.m4a.part_urls')
+    if os.path.isfile('temp/'+temp_title+'.mp4.part_urls.txt'):
+        os.remove('temp/'+temp_title+'.mp4.part_urls.txt')
+    if os.path.isfile('temp/'+temp_title+'.m4a.part_urls.txt'):
+        os.remove('temp/'+temp_title+'.m4a.part_urls.txt')
+    if os.path.isfile('temp/'+temp_title+'.m4a'):
+        os.remove('temp/'+temp_title+'.m4a')
+    if os.path.isfile('temp/'+temp_title+'.mp4'):
+        os.remove('temp/'+temp_title+'.mp4')
 
 video_mpd = ""
 video_xml = ""
 keys = []
+# Generate random temporary video title to prevent issues with certain characters
+temp_title = "rte_video_"+str(randint(1, 999))
+
+if not os.path.isfile("decryptor.crx"):
+    print("Error: Cannot run script, decryptor is not in folder. Please read the README.")
+    print("If you do have it, ensure it's titled 'decryptor.crx'.")
+    sys.exit(1)
+
 try:
     url = sys.argv[1]
 except IndexError:
     print("Error: Please enter an RTÉ Player URL to download.")
-    sys.exit()
-
-path_to_crx = "decryptor.crx"
+    sys.exit(2)
 
 if not os.path.isdir('temp'):
     os.mkdir('temp')
@@ -75,10 +77,8 @@ if not os.path.isdir('Downloads'):
 print("Loading page...")
 
 options = webdriver.ChromeOptions()
-if headless:
-    options.add_argument("headless")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_extension(path_to_crx)
+options.add_extension("decryptor.crx")
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 
@@ -86,18 +86,18 @@ try:
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, 'onetrust-policy-text')))
 except selenium.common.exceptions.TimeoutException:
     print("Request timed out, try again later.")
-    sys.exit()
+    sys.exit(3)
 
 print("Accepting necessary cookies...")
 try:
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'onetrust-pc-btn-handler')))
     driver.find_element_by_class_name('cookie-setting-link').click()
     time.sleep(0.5)
     driver.find_element_by_class_name('save-preference-btn-handler').click()
-    time.sleep(0.5)
 except selenium.common.exceptions.TimeoutException:
     driver.quit()
     print("Request timed out, try again.")
-    sys.exit()
+    sys.exit(4)
 
 WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'btn')))
 
@@ -108,7 +108,7 @@ try:
     driver.find_elements_by_class_name('ic-play-white')[0].click()
 except selenium.common.exceptions.NoSuchElementException:
     print("Request timed out, try again later.")
-    sys.exit()
+    sys.exit(5)
 time.sleep(2)
 
 # Bypass mature content pop-up if needed
@@ -139,26 +139,28 @@ for request in driver.requests:
 
 try:
     req = requests.get(video_xml, allow_redirects=True)
-    video_embed_xml = BeautifulSoup(req.content, "lxml")
-    video_title_element = video_embed_xml.find(
-            lambda tag: tag.name == "meta" and "title" in tag.attrs["name"]
-        )
+    html = codecs.decode(req.content, "utf-8")
+    video_embed_xml = BeautifulSoup(html, "lxml")
+    video_title_element = video_embed_xml.find(lambda tag: tag.name == "meta" and "title" in tag.attrs["name"])
 except requests.exceptions.MissingSchema:
     driver.quit()
     print("Request timed out, try again.")
-    sys.exit()
+    sys.exit(6)
 
 print()
 try:
     video_title = video_title_element.attrs["content"]
     print(u"Video title = " + video_title)
+    # Remove characters forbidden on Windows
+    video_title = re.sub(r'[\\/*?:"<>|]',"",video_title)
 except AttributeError:
     video_title = "RTE-Player-Video"
     print("Unable to extract video title, using placeholder title instead.")
+    print("I strongly encourage you to rename this file after it downloads as it may be overwritten.")
 
 if video_mpd == "":
     print("MPD URL not found, try again.")
-    sys.exit()
+    sys.exit(7)
 else:
     print()
     print("Found MPD URL: " + video_mpd)
@@ -186,13 +188,13 @@ except IndexError:
     if yes_or_no("Would you like to download anyway?"):
         download_video()
         print("Merging videos together...")
-        os.system('binaries\mkvmerge.exe -o "Downloads/'+video_title+'.mkv" "temp/'+video_title+'.mp4" "temp/'+video_title+'.m4a"')
+        os.system(r'binaries\mkvmerge.exe -o "Downloads/'+temp_title+'.mkv" "temp/'+temp_title+'.mp4" "temp/'+temp_title+'.m4a"')
         cleanup()
         print("Finished!")
         sys.exit(0)
     else:
         print("Okay, exiting.")
-        sys.exit()
+        sys.exit(8)
 
 print("Obtained five possible decryption keys (KID:Key):")
 print("Key 1: " + kid_key1)
@@ -206,54 +208,54 @@ download_video()
 
 # Decryption stage
 print("Decrypting video and audio with first key...")
-os.system('binaries\mp4decrypt.exe --key '+kid_key1+' "temp/'+video_title+'.mp4" "temp/'+video_title+'-key1.mp4"')
-os.system('binaries\mp4decrypt.exe --key '+kid_key1+' "temp/'+video_title+'.m4a" "temp/'+video_title+'-key1.m4a"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key1+' "temp/'+temp_title+'.mp4" "temp/'+temp_title+'-key1.mp4"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key1+' "temp/'+temp_title+'.m4a" "temp/'+temp_title+'-key1.m4a"')
 
 print("Decrypting video and audio with second key...")
-os.system('binaries\mp4decrypt.exe --key '+kid_key2+' "temp/'+video_title+'.mp4" "temp/'+video_title+'-key2.mp4"')
-os.system('binaries\mp4decrypt.exe --key '+kid_key2+' "temp/'+video_title+'.m4a" "temp/'+video_title+'-key2.m4a"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key2+' "temp/'+temp_title+'.mp4" "temp/'+temp_title+'-key2.mp4"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key2+' "temp/'+temp_title+'.m4a" "temp/'+temp_title+'-key2.m4a"')
 
 print("Decrypting video and audio with third key...")
-os.system('binaries\mp4decrypt.exe --key '+kid_key3+' "temp/'+video_title+'.mp4" "temp/'+video_title+'-key3.mp4"')
-os.system('binaries\mp4decrypt.exe --key '+kid_key3+' "temp/'+video_title+'.m4a" "temp/'+video_title+'-key3.m4a"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key3+' "temp/'+temp_title+'.mp4" "temp/'+temp_title+'-key3.mp4"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key3+' "temp/'+temp_title+'.m4a" "temp/'+temp_title+'-key3.m4a"')
 
 print("Decrypting video and audio with fourth key...")
-os.system('binaries\mp4decrypt.exe --key '+kid_key4+' "temp/'+video_title+'.mp4" "temp/'+video_title+'-key4.mp4"')
-os.system('binaries\mp4decrypt.exe --key '+kid_key4+' "temp/'+video_title+'.m4a" "temp/'+video_title+'-key4.m4a"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key4+' "temp/'+temp_title+'.mp4" "temp/'+temp_title+'-key4.mp4"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key4+' "temp/'+temp_title+'.m4a" "temp/'+temp_title+'-key4.m4a"')
 
 print("Decrypting video and audio with fifth key...")
-os.system('binaries\mp4decrypt.exe --key '+kid_key5+' "temp/'+video_title+'.mp4" "temp/'+video_title+'-key5.mp4"')
-os.system('binaries\mp4decrypt.exe --key '+kid_key5+' "temp/'+video_title+'.m4a" "temp/'+video_title+'-key5.m4a"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key5+' "temp/'+temp_title+'.mp4" "temp/'+temp_title+'-key5.mp4"')
+os.system(r'binaries\mp4decrypt.exe --key '+kid_key5+' "temp/'+temp_title+'.m4a" "temp/'+temp_title+'-key5.m4a"')
 
 # Compare file size of each output to determine which key was the correct one
 print("Determining video decrypted with correct key...")
-path1_mp4 = os.path.abspath('temp/'+video_title+'-key1.mp4')
-path2_mp4 = os.path.abspath('temp/'+video_title+'-key2.mp4')
-path3_mp4 = os.path.abspath('temp/'+video_title+'-key3.mp4')
-path4_mp4 = os.path.abspath('temp/'+video_title+'-key4.mp4')
-path5_mp4 = os.path.abspath('temp/'+video_title+'-key5.mp4')
+path1_mp4 = os.path.abspath('temp/'+temp_title+'-key1.mp4')
+path2_mp4 = os.path.abspath('temp/'+temp_title+'-key2.mp4')
+path3_mp4 = os.path.abspath('temp/'+temp_title+'-key3.mp4')
+path4_mp4 = os.path.abspath('temp/'+temp_title+'-key4.mp4')
+path5_mp4 = os.path.abspath('temp/'+temp_title+'-key5.mp4')
 
-path1_mp4_size = os.path.getsize('temp/'+video_title+'-key1.mp4')
-path2_mp4_size = os.path.getsize('temp/'+video_title+'-key2.mp4')
-path3_mp4_size = os.path.getsize('temp/'+video_title+'-key3.mp4')
-path4_mp4_size = os.path.getsize('temp/'+video_title+'-key4.mp4')
-path5_mp4_size = os.path.getsize('temp/'+video_title+'-key5.mp4')
+path1_mp4_size = os.path.getsize('temp/'+temp_title+'-key1.mp4')
+path2_mp4_size = os.path.getsize('temp/'+temp_title+'-key2.mp4')
+path3_mp4_size = os.path.getsize('temp/'+temp_title+'-key3.mp4')
+path4_mp4_size = os.path.getsize('temp/'+temp_title+'-key4.mp4')
+path5_mp4_size = os.path.getsize('temp/'+temp_title+'-key5.mp4')
 
 sizes_mp4 = [path1_mp4_size, path2_mp4_size, path3_mp4_size, path4_mp4_size, path5_mp4_size]
 counter_mp4 = Counter(sizes_mp4)
 output_mp4 = sizes_mp4.index(min(counter_mp4, key=counter_mp4.get))
 
-path1_m4a = os.path.abspath('temp/'+video_title+'-key1.m4a')
-path2_m4a = os.path.abspath('temp/'+video_title+'-key2.m4a')
-path3_m4a = os.path.abspath('temp/'+video_title+'-key3.m4a')
-path4_m4a = os.path.abspath('temp/'+video_title+'-key4.m4a')
-path5_m4a = os.path.abspath('temp/'+video_title+'-key5.m4a')
+path1_m4a = os.path.abspath('temp/'+temp_title+'-key1.m4a')
+path2_m4a = os.path.abspath('temp/'+temp_title+'-key2.m4a')
+path3_m4a = os.path.abspath('temp/'+temp_title+'-key3.m4a')
+path4_m4a = os.path.abspath('temp/'+temp_title+'-key4.m4a')
+path5_m4a = os.path.abspath('temp/'+temp_title+'-key5.m4a')
 
-path1_m4a_size = os.path.getsize('temp/'+video_title+'-key1.m4a')
-path2_m4a_size = os.path.getsize('temp/'+video_title+'-key2.m4a')
-path3_m4a_size = os.path.getsize('temp/'+video_title+'-key3.m4a')
-path4_m4a_size = os.path.getsize('temp/'+video_title+'-key4.m4a')
-path5_m4a_size = os.path.getsize('temp/'+video_title+'-key5.m4a')
+path1_m4a_size = os.path.getsize('temp/'+temp_title+'-key1.m4a')
+path2_m4a_size = os.path.getsize('temp/'+temp_title+'-key2.m4a')
+path3_m4a_size = os.path.getsize('temp/'+temp_title+'-key3.m4a')
+path4_m4a_size = os.path.getsize('temp/'+temp_title+'-key4.m4a')
+path5_m4a_size = os.path.getsize('temp/'+temp_title+'-key5.m4a')
 
 sizes_m4a = [path1_m4a_size, path2_m4a_size, path3_m4a_size, path4_m4a_size, path5_m4a_size]
 counter_m4a = Counter(sizes_m4a)
@@ -265,24 +267,26 @@ else:
     print("Key mismatch!")
     print("Something went wrong, unable to automatically determine correct decryption key.")
     print("Please check the files manually in the temp folder to see which key was the correct one. You can merge them manually.")
-    sys.exit()
+    sys.exit(9)
 
-os.remove('temp/'+video_title+'.mp4')
-os.remove('temp/'+video_title+'.m4a')
+os.remove('temp/'+temp_title+'.mp4')
+os.remove('temp/'+temp_title+'.m4a')
 
 for i in range(5):
     i += 1
     if i == output_mp4+1:
-        os.rename('temp/'+video_title+'-key'+str(i)+'.mp4', 'temp/'+video_title+'.mp4')
-        os.rename('temp/'+video_title+'-key'+str(i)+'.m4a', 'temp/'+video_title+'.m4a')
+        os.rename('temp/'+temp_title+'-key'+str(i)+'.mp4', 'temp/'+temp_title+'.mp4')
+        os.rename('temp/'+temp_title+'-key'+str(i)+'.m4a', 'temp/'+temp_title+'.m4a')
     else:
-        os.remove('temp/'+video_title+'-key'+str(i)+'.mp4')
-        os.remove('temp/'+video_title+'-key'+str(i)+'.m4a')
+        os.remove('temp/'+temp_title+'-key'+str(i)+'.mp4')
+        os.remove('temp/'+temp_title+'-key'+str(i)+'.m4a')
 
 # Merging video and audio into Matroska
 
 print("Merging videos together...")
-os.system('binaries\mkvmerge.exe -o "Downloads/'+video_title+'.mkv" "temp/'+video_title+'.mp4" "temp/'+video_title+'.m4a"')
+os.system(r'binaries\mkvmerge.exe -o "Downloads/'+temp_title+'.mkv" "temp/'+temp_title+'.mp4" "temp/'+temp_title+'.m4a"')
+os.rename('Downloads/'+temp_title+'.mkv', 'Downloads/'+video_title+'.mkv')
+print("Saved as: "+video_title+".mkv")
 
 # Cleaning up leftover files
 cleanup()
